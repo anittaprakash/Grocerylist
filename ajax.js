d4e6db0451c12b7858d2695f58432d0dd09a12da
Anitta <anittaprakash17@gmail.com>
2021-09-12 20:31:55 +0530

    First Commit:Fisrt Version

---
+++ ajax.js
@@ -0,0 +1,41 @@
+function ajax()
+{
+
+    var xhttp= new XMLHttpRequest();
+    xhttp.onreadystatechange=function(){
+        if(this.readyState==4&&this.status==200){
+            var response=JSON.parse(this.responseText);
+            var Grocery=response.Grocery;
+            var output="";
+            
+                output +="<tr>"+ 
+                    "<th>"+ "Sl No" +"</th>"+
+                    "<th>"+ "Items "+"</th>"+
+                    "<th>"+ "Quantity" +"</th>"+
+                    "<th>"+ "Unit" +"</th>"+
+                    "<th>"+ "Category" +"</th>"+
+                    "<th>"+ "Notes" +"</th>"
+                +"<tr>";
+              
+
+            for(var i=0;i<Grocery.length;i++){
+            output +="<tr>"+ 
+                "<td>"+ Grocery[i].SINo +"</td>"+
+                "<td>"+ Grocery[i].Item +"</td>"+
+                "<td>"+ Grocery[i].Quatity +"</td>"+
+                "<td>"+ Grocery[i].Unit +"</td>"+
+                "<td>"+ Grocery[i].category +"</td>"+
+                "<td>"+ Grocery[i].Note +"</td>"
+            +"<tr>";
+               
+           
+            }
+            // console.log(output);
+            document.getElementById("tbl").innerHTML=output;
+           
+         }
+    
+    }
+    xhttp.open("GET","products.json",true);
+    xhttp.send();
+}
